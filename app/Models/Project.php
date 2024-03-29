65e38d4f3be5f5498bc404c2a386766379287a86
Polvere243 <danielercoli@gmail.com>
2024-03-29 12:27:26 +0100

    established relation between two tables

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Type;
 
 class Project extends Model
 {
@@ -12,4 +13,9 @@ class Project extends Model
     use SoftDeletes;
 
     protected $fillable = ['title', 'description'];
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }

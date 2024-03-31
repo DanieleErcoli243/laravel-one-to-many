321486aee7d4ca30c3626800b98b49bd5a406320
Polvere243 <danielercoli@gmail.com>
2024-03-31 18:20:31 +0200

    copied and pasted the index

---
+++ resources/views/admin/types/index.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app')
+
+@section('title', 'Tipi')
+
+
+@section('content')
+
+<table class="text-center"> 
+    <thead>
+        <tr>
+            <th>Titolo</th>
+            <th>Tipo</th>
+            <th>Descrizione</th>
+            <th>Immagine</th>
+            <th>
+                <a href="{{ route('admin.projects.create') }}">
+                  <i class="fas fa-plus"></i>
+                </a>
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+        @forelse($projects as $project)
+        <tr>
+            <td>{{ $project->title }}</td>
+            <td>{{ $project->type ? $project->type->label : '-' }}</td>
+            <td>{{ $project->description }}</td>
+            <td>{{ $project->image }}</td>
+            <td>
+                <div class="action-btn">
+                    <a href="{{ route('admin.projects.show', $project->id) }}"><i class="fas fa-eye"></i></a>
+                    <a href="{{ route('admin.projects.edit', $project->id) }}"><i class="fas fa-pencil"></i></a>
+                    <form action="{{ route('admin.projects.destroy', $project->id) }}" method="POST" class="deleting-form">
+                        @csrf
+                        @method('DELETE')
+                        <button class="delete" type="submit">
+                            <i class="fas fa-trash-can"></i>
+                        </button>
+                    </form> 
+                </div>   
+            </td>
+            @empty
+            <h1>Non ci sono progetti</h1>
+            @endforelse
+        </tr>
+    </tbody>
+    
+</table>
+
+@endsection
+
+@section('scripts')
+@vite('resources/js/delete-confirmation.js')
+@endsection
